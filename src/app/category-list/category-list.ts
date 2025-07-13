cacc54fd56731211464ee66399050e5a9679652c
tushar242001 <tusharadling24@gmail.com>
2025-07-13 21:10:55 +0530

    Added images

--- src/app/category-list/category-list.ts
+++ src/app/category-list/category-list.ts
@@ -1,8 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-category-list',
-  imports: [],
+  imports: [CommonModule,RouterModule],
   templateUrl: './category-list.html',
   styleUrl: './category-list.css'
 })
